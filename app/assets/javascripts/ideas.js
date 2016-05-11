0a2f2d6baabd446ee293314922439aa4418b6886
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-11 12:31:33 -0600

    Wrapping up this branch

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   $('body').on("click", "input#delete-idea", deleteIdea);
   $('body').on("blur", "h3.displayed-idea-title", editTitle);
   $('body').on("blur", "p.displayed-idea-body", editBody);
+var $ideaTitles = $('.idea-title');
 })
 
 function fetchIdeas(event) {
@@ -121,103 +122,3 @@ function deleteIdea(){
     }
   })
 };
-
-
-// + "<input type='button' value='Edit Idea' class='btn btn-primary' id='edit-idea'>"
-// function editIdea(){
-//   event.preventDefault();
-//   var ideaId = this.parentElement.getAttribute("data-idea-id")
-//   var ideaParams = {idea: {title: $("#displayed-idea-title-" + ideaId), body: $("#displayed-idea-body-" + ideaId)}}
-// debugger;
-//   $.ajax({
-//     url: "api/v1/ideas/" + ideaId,
-//     method: "PUT",
-//     dataType: "json",
-//     data: ideaParams,
-//     success: fetchIdeas,
-//     error: function(){
-//       alert("Something went wrong")
-//     }
-//   })
-// };
-
-
-
-
-
-// $(document).ready(function(){
-//
-//   window.onload = fetchPosts
-//   $("#create-post").on("click", createPost)
-//   $("button[name=button-fetch]").on("click", fetchPosts)
-//   $("body").on('click', "a.delete-post", deletePost)
-//   setInterval(fetchPosts, 5000)
-//
-// })
-//
-// function fetchPosts(){
-//   $.getJSON( "https://birdeck.herokuapp.com/api/v1/posts.json", successfulPost
-//   )
-// }
-// // function fetchPosts(){
-// //   $.ajax({
-// //     url: "https://birdeck.herokuapp.com/api/v1/posts.json",
-// //     method: "GET",
-// //     dataType: "json",
-// //     success: successfulPost,
-// //     error: function(){
-// //       alert("Something went wrong")
-// //     }
-// //   })
-// // }
-//
-// function createPost(){
-//   var postParams = {post: {description: $("#post-description").val()}}
-//   $.ajax({
-//     url: "https://birdeck.herokuapp.com/api/v1/posts.json",
-//     method: "POST",
-//     dataType: "json",
-//     data: postParams,
-//     success: successfulPosts,
-//   })
-// }
-//
-// function deletePost(){
-//   event.preventDefault();
-//   // var post_id = $(this).data("post-id")
-//   // var post_id = $(this).closest(".post").data("post-id")
-//   var post_id = $(this).parents(".post").data("post-id")
-//
-//   $.ajax({
-//     url: "https://birdeck.herokuapp.com/api/v1/posts/" + post_id + ".json",
-//     method: "DELETE",
-//     success: function(){
-//       $(".post[data-post-id=" + post_id + "]").remove()
-//     }
-//   })
-// }
-//
-// function updatePosts(post){
-//   $("#latest-posts")
-//   .append("<div class=post data-post-id="
-//   + post.id
-//   + "><h6>Published:"
-//   + post.created_at
-//   + "</h6><p>"
-//   + post.description
-//   + "</p><a class='delete-post' href='#'>Delete</a></div>")
-// }
-//
-// function successfulPost(posts){
-//   $("#latest-posts").html('');
-//   $(posts).each(function(index, post){
-//     $("#latest-posts")
-//     .append("<div class=post data-post-id="
-//     + post.id
-//     + "><h6>Published:"
-//     + post.created_at
-//     + "</h6><p>"
-//     + post.description
-//     + "</p><a class='delete-post' href='#'>Delete</a></div>")
-//   })
-// }
