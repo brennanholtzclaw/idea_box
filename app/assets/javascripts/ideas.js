3d08fed5b65606c6ce6d39a9096f9d80aa140507
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-11 13:50:48 -0600

    Implement ability to filter ideas by title
    * Used filter bar with keyup listener
    * Changed div and individual elements for each idea to match jQuery
    * There may now be some extra information in div's

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
   window.onload = fetchIdeas
   $("#create-idea").on("click", createIdea);
+  $("#idea_filter_name").on("keyup", filterIdeas)
   $('body').on("click", "input#delete-idea", deleteIdea);
   $('body').on("blur", "h3.displayed-idea-title", editTitle);
   $('body').on("blur", "p.displayed-idea-body", editBody);
-var $ideaTitles = $('.idea-title');
 })
+
+
+function filterIdeas() {
+  var currentName = this.value;
+  var ideaTitles = $('.displayed-idea-title');
+  ideaTitles.each(function(index, idea){
+    if ($(idea).text().indexOf(currentName) !== -1) {
+      $(idea.parentElement).show();
+    } else {
+      $(idea.parentElement).hide();
+    }
+  })
+}
 
 function fetchIdeas(event) {
   $.ajax({
@@ -38,9 +51,11 @@ function prependIdea(idea) {
   $(".whole-idea").prepend(
     "<div class='idea' data-idea-id="
     + idea.id
-    + "><hr><h3 class='displayed-idea-title' id='idea-title-"
+    + "><hr><h3 class='displayed-idea-title idea' id='idea-title-"
     + idea.id
-    +"' contentEditable='true'>"
+    +"' contentEditable='true' data-title='"
+    + idea.title
+    +"'>"
     + idea.title
     + "</h3><p class='displayed-idea-body id='idea-body-"
     + idea.id
@@ -61,7 +76,9 @@ function showIdeas(results) {
       + idea.id
       + "><hr><h3 class='displayed-idea-title' id='idea-title-"
       + idea.id
-      +"' contentEditable='true'>"
+      +"' contentEditable='true' data-title='"
+      + idea.title
+      +"'>"
       + idea.title
       + "</h3><p class='displayed-idea-body id='idea-body-"
       + idea.id
@@ -122,3 +139,117 @@ function deleteIdea(){
     }
   })
 };
+
+// var $robots = $('.robot');
+
+// $("#robot_filter_name").on("keyup", function(){
+//   var currentName = this.value;
+//
+//   $robots.each(function(index, robot){
+//     $robot = $(robot);
+//     if ($robot.data("name").indexOf(currentName) !== -1) {
+//       $robot.show();
+//     } else {
+//       $robot.hide();
+//     }
+//   });
+// });
+//
+// + "<input type='button' value='Edit Idea' class='btn btn-primary' id='edit-idea'>"
+// function editIdea(){
+//   event.preventDefault();
+//   var ideaId = this.parentElement.getAttribute("data-idea-id")
+//   var ideaParams = {idea: {title: $("#displayed-idea-title-" + ideaId), body: $("#displayed-idea-body-" + ideaId)}}
+// debugger;
+//   $.ajax({
+//     url: "api/v1/ideas/" + ideaId,
+//     method: "PUT",
+//     dataType: "json",
+//     data: ideaParams,
+//     success: fetchIdeas,
+//     error: function(){
+//       alert("Something went wrong")
+//     }
+//   })
+// };
+
+
+
+
+
+// $(document).ready(function(){
+//
+//   window.onload = fetchPosts
+//   $("#create-post").on("click", createPost)
+//   $("button[name=button-fetch]").on("click", fetchPosts)
+//   $("body").on('click', "a.delete-post", deletePost)
+//   setInterval(fetchPosts, 5000)
+//
+// })
+//
+// function fetchPosts(){
+//   $.getJSON( "https://birdeck.herokuapp.com/api/v1/posts.json", successfulPost
+//   )
+// }
+// // function fetchPosts(){
+// //   $.ajax({
+// //     url: "https://birdeck.herokuapp.com/api/v1/posts.json",
+// //     method: "GET",
+// //     dataType: "json",
+// //     success: successfulPost,
+// //     error: function(){
+// //       alert("Something went wrong")
+// //     }
+// //   })
+// // }
+//
+// function createPost(){
+//   var postParams = {post: {description: $("#post-description").val()}}
+//   $.ajax({
+//     url: "https://birdeck.herokuapp.com/api/v1/posts.json",
+//     method: "POST",
+//     dataType: "json",
+//     data: postParams,
+//     success: successfulPosts,
+//   })
+// }
+//
+// function deletePost(){
+//   event.preventDefault();
+//   // var post_id = $(this).data("post-id")
+//   // var post_id = $(this).closest(".post").data("post-id")
+//   var post_id = $(this).parents(".post").data("post-id")
+//
+//   $.ajax({
+//     url: "https://birdeck.herokuapp.com/api/v1/posts/" + post_id + ".json",
+//     method: "DELETE",
+//     success: function(){
+//       $(".post[data-post-id=" + post_id + "]").remove()
+//     }
+//   })
+// }
+//
+// function updatePosts(post){
+//   $("#latest-posts")
+//   .append("<div class=post data-post-id="
+//   + post.id
+//   + "><h6>Published:"
+//   + post.created_at
+//   + "</h6><p>"
+//   + post.description
+//   + "</p><a class='delete-post' href='#'>Delete</a></div>")
+// }
+//
+// function successfulPost(posts){
+//   $("#latest-posts").html('');
+//   $(posts).each(function(index, post){
+//     $("#latest-posts")
+//     .append("<div class=post data-post-id="
+//     + post.id
+//     + "><h6>Published:"
+//     + post.created_at
+//     + "</h6><p>"
+//     + post.description
+//     + "</p><a class='delete-post' href='#'>Delete</a></div>")
+//   })
+// }
