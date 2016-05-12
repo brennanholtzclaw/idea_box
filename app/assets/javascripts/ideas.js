42cc63ea54597687facb94978f2281a52c6c4a25
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-11 18:49:33 -0600

    Pull ideaDiv into its own function

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -7,12 +7,6 @@ $(document).ready(function(){
   $('body').on("click", "input#thumbs-down", thumbsDown);
   $('body').on("blur", "h3.displayed-idea-title", editTitle);
   $('body').on("blur", "p.displayed-idea-body", editBody);
-  // $('body').keydown("h3.displayed-idea-title", function(e) {
-  //   if (e.keyCode === 13) {
-  //     return false
-  //     editTitle
-  //   }
-  // });
 })
 
 function fetchIdeas(event) {
@@ -27,53 +21,35 @@ function fetchIdeas(event) {
   })
 }
 
-function prependIdea(idea) {
-  $(".whole-idea").prepend(
-    "<div class='idea' data-idea-id="
-    + idea.id
-    + " data-quality='"
-    + idea.quality
-    + "'><hr><h3 class='displayed-idea-title idea' id='idea-title-"
-    + idea.id
-    + "' contentEditable='true'>"
-    + idea.title
-    + "</h3><p class='displayed-idea-body id='idea-body-"
-    + idea.id
-    + "' contentEditable='true'>"
-    + idea.body
-    + "</p><h4>Quality: "
-    + idea.quality
-    + "</h4>"
-    + "<input type='button' value='Delete Idea' class='btn btn-danger' id='delete-idea'>  "
-    + "<input type='button' value='Thumbs Up' class='btn btn-success' id='thumbs-up'>  "
-    + "<input type='button' value='Thumbs Down' class='btn btn-warning' id='thumbs-down'>  "
-    + "</div>"
-  )
-}
-
 function showIdeas(results) {
   $(".whole-idea").html("")
   results.forEach(function(idea){
-    $(".whole-idea").prepend(
-      "<div class='idea' data-idea-id="
-      + idea.id
-      + " data-quality='"
-      + idea.quality
-      + "'><hr><h3 class='displayed-idea-title idea' id='idea-title-"
-      + idea.id
-      + "' contentEditable='true'>"
-      + idea.title
-      + "</h3><p class='displayed-idea-body id='idea-body-"
-      + idea.id
-      + "' contentEditable='true'>"
-      + idea.body
-      + "</p><h4>Quality: "
-      + idea.quality
-      + "</h4>"
-      + "<input type='button' value='Delete Idea' class='btn btn-danger' id='delete-idea'>  "
-      + "<input type='button' value='Thumbs Up' class='btn btn-success' id='thumbs-up'>  "
-      + "<input type='button' value='Thumbs Down' class='btn btn-warning' id='thumbs-down'>  "
-      + "</div>"
-    )
+    $(".whole-idea").prepend(ideaDiv(idea))
   })
 }
+
+function prependIdea(idea) {
+  $(".whole-idea").prepend(ideaDiv(idea))
+}
+
+function ideaDiv(idea) {
+  return "<div class='idea' data-idea-id="
+  + idea.id
+  + " data-quality='"
+  + idea.quality
+  + "'><hr><h3 class='displayed-idea-title idea' id='idea-title-"
+  + idea.id
+  + "' contentEditable='true'>"
+  + idea.title
+  + "</h3><p class='displayed-idea-body id='idea-body-"
+  + idea.id
+  + "' contentEditable='true'>"
+  + idea.body
+  + "</p><h4>Quality: "
+  + idea.quality
+  + "</h4>"
+  + "<input type='button' value='Delete Idea' class='btn btn-danger' id='delete-idea'>  "
+  + "<input type='button' value='Thumbs Up' class='btn btn-success' id='thumbs-up'>  "
+  + "<input type='button' value='Thumbs Down' class='btn btn-warning' id='thumbs-down'>  "
+  + "</div>"
+};
